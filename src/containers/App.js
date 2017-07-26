e0a95948412fc97d42bd053a5d944ea7f4134a72
shapikcool@gmail.com <shapikcool@gmail.com>
2017-07-26 10:50:35 +0300

    first commit

---
+++ src/containers/App.js
@@ -0,0 +1,23 @@
+import React, {Component} from 'react';
+import { connect } from 'react-redux';
+import User from '../components/User'
+import Page from '../components/Page'
+
+class App extends Component {
+   render(){
+       const {user, page} = this.props
+       return <div>
+           <User name={user.name}/>
+           <Page photos={page.photos} year={page.year}/>
+           </div>
+   }
+}
+
+function mapStateToProps (state) {
+  return {
+    user: state.user,
+    page: state.page
+  }
+}
+
+export default connect(mapStateToProps)(App)
